ef136659832e1f2a82f29d3afce1be90a5459d31
vph <vph.persson@gmail.com>
2025-02-17 16:03:52 +0100

    Add crypto.

---
+++ pkg/crypto/crypto.go
@@ -0,0 +1,67 @@
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"crypto/x509"
+	"encoding/pem"
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
+	gcpUtilsCryptoErrors "github.com/altshiftab/gcp_utils/pkg/crypto/errors"
+)
+
+func ParseCertificateMaterial(pemData []byte) (*ecdsa.PrivateKey, []*x509.Certificate, error) {
+	if len(pemData) == 0 {
+		return nil, nil, nil
+	}
+
+	var key *ecdsa.PrivateKey
+	var certificates []*x509.Certificate
+
+	rest := pemData
+	for {
+		block, remaining := pem.Decode(rest)
+		if block == nil {
+			break
+		}
+
+		blockType := block.Type
+		blockBytes := block.Bytes
+
+		switch blockType {
+		case "EC PRIVATE KEY":
+			if key != nil {
+				return nil, nil, gcpUtilsCryptoErrors.ErrMultipleCertificateKeys
+			}
+
+			parsedKey, err := x509.ParseECPrivateKey(blockBytes)
+			if err != nil {
+				return nil, nil, &motmedelErrors.CauseError{
+					Message: "An error occurred when parsing block bytes as a private key.",
+					Cause:   err,
+				}
+			}
+			key = parsedKey
+		case "CERTIFICATE":
+			certificate, err := x509.ParseCertificate(blockBytes)
+			if err != nil {
+				return nil, nil, &motmedelErrors.CauseError{
+					Message: "An error occurred when parsing block bytes as a certificate.",
+					Cause:   err,
+				}
+			}
+			if certificate == nil {
+				continue
+			}
+
+			certificates = append(certificates, certificate)
+		default:
+			return nil, nil, &motmedelErrors.InputError{
+				Message: "An unexpected PEM block type was encountered.",
+				Input:   blockBytes,
+			}
+		}
+
+		rest = remaining
+	}
+
+	return key, certificates, nil
+}
---
+++ pkg/crypto/errors/errors.go
@@ -0,0 +1,7 @@
+package errors
+
+import "errors"
+
+var (
+	ErrMultipleCertificateKeys = errors.New("multiple certificate keys")
+)
