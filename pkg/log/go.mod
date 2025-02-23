84bc567720df763c24efcfabb310479860ea2752
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-02-23 23:30:57 +0100

    Update for new logging

--- pkg/log/go.mod
+++ pkg/log/go.mod
@@ -1,16 +1,17 @@
 module github.com/altshiftab/gcp_utils/pkg/log
 
-go 1.23.5
+go 1.24.0
 
 require (
-	github.com/Motmedel/ecs_go v0.0.54
-	github.com/Motmedel/gcp_logging_go v0.0.4
-	github.com/Motmedel/utils_go v0.0.110
-	github.com/altshiftab/gcp_utils v0.0.2
+	github.com/Motmedel/gcp_logging_go v0.0.5
+	github.com/Motmedel/utils_go v0.0.133
+	github.com/Motmedel/utils_go/pkg/http/log v0.0.4
+	github.com/altshiftab/gcp_utils v0.0.4
 	snqk.dev/slog/meld v0.0.0-20240701183407-595424398869
 )
 
 require (
+	github.com/Motmedel/ecs_go v0.0.55 // indirect
 	github.com/Motmedel/utils_go/pkg/net/domain_breakdown v0.0.3 // indirect
 	golang.org/x/net v0.33.0 // indirect
 )
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,11 +1,10 @@
 package log
 
 import (
-	"github.com/Motmedel/ecs_go/ecs"
+	"fmt"
 	"github.com/Motmedel/gcp_logging_go/gcp_logging"
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
-	motmedelHttpTypes "github.com/Motmedel/utils_go/pkg/http/types"
-	motmedelJson "github.com/Motmedel/utils_go/pkg/json"
+	motmedelHttpLog "github.com/Motmedel/utils_go/pkg/http/log"
 	motmedelLog "github.com/Motmedel/utils_go/pkg/log"
 	altshiftGcpUtilsEnv "github.com/altshiftab/gcp_utils/pkg/env"
 	"io"
@@ -15,87 +14,34 @@ import (
 	"snqk.dev/slog/meld"
 )
 
-func getHttpLogAttrs(httpContext *motmedelHttpTypes.HttpContext, logger *slog.Logger) []any {
-	if httpContext == nil {
-		return nil
-	}
-
-	if logger == nil {
-		logger = slog.Default()
-	}
-
-	var logAttrs []any
-	if httpContext != nil {
-		ecsBase, err := ecs.ParseHttpContext(httpContext, ecs.DefaultMaskedHeaderExtractor)
-		if err != nil {
-			motmedelLog.LogError("An error occurred when parsing an HTTP context into ECS.", err, logger)
-		}
-		if ecsBase != nil {
-			if baseMap, ok := motmedelJson.Jsonify(ecsBase).(map[string]any); ok {
-				logAttrs = append(logAttrs, motmedelLog.AttrsFromMap(baseMap)...)
-			} else {
-				msg := "An ECS base object could not be converted into a jsonified map."
-				motmedelLog.LogError(msg, &motmedelErrors.Error{Message: msg, Input: ecsBase}, logger)
-			}
-		}
-
-		gcpLogEntry, err := gcp_logging.ParseHttp(httpContext.Request, httpContext.Response)
-		if err != nil {
-			motmedelLog.LogError("An error occurred when parsing HTTP data into GCP LogEntry.", err, logger)
-		}
-		if gcpLogEntry != nil {
-			if gcpLogEntryMap, ok := motmedelJson.Jsonify(ecsBase).(map[string]any); ok {
-				logAttrs = append(logAttrs, motmedelLog.AttrsFromMap(gcpLogEntryMap)...)
-			} else {
-				msg := "A GCP LogEntry object could not be converted into a jsonified map."
-				motmedelLog.LogError(msg, &motmedelErrors.Error{Message: msg, Input: gcpLogEntry}, logger)
-			}
-		}
-	}
-
-	return logAttrs
-}
-
-func LogFatalHttpErrorWithExitingMessage(
-	message string,
-	err error,
-	logger *slog.Logger,
-	httpContext *motmedelHttpTypes.HttpContext,
-) {
-	motmedelLog.LogFatalWithExitingMessage(message, err, logger.With(getHttpLogAttrs(httpContext, logger)...))
-}
-
-func LogHttpError(message string, err error, logger *slog.Logger, httpContext *motmedelHttpTypes.HttpContext) {
-	motmedelLog.LogError(message, err, logger.With(getHttpLogAttrs(httpContext, logger)...))
-}
-
-func LogHttpInfo(message string, logger *slog.Logger, httpContext *motmedelHttpTypes.HttpContext) {
-	logger.Info(message, getHttpLogAttrs(httpContext, logger)...)
-}
-
-func LogHttpDebug(message string, logger *slog.Logger, httpContext *motmedelHttpTypes.HttpContext) {
-	logger.Debug(message, getHttpLogAttrs(httpContext, logger)...)
-}
-
 func MakeLoggerWithWriter(writer io.Writer) (*motmedelLog.Logger, error) {
 	var level slog.Level
-	if err := level.UnmarshalText([]byte(altshiftGcpUtilsEnv.GetLogLevelWithDefault())); err != nil {
-		return nil, &motmedelErrors.Error{
-			Message: "An error occurred when obtaining the default log level.",
-			Cause:   err,
-		}
+	levelTextData := []byte(altshiftGcpUtilsEnv.GetLogLevelWithDefault())
+	if err := level.UnmarshalText(levelTextData); err != nil {
+		return nil, motmedelErrors.MakeError(
+			fmt.Errorf("slog level unmarshal text: %w", err),
+			levelTextData,
+		)
 	}
 
+	// TODO: I'm not sure about the order; must be specific for meld to work?
 	outLogger := slog.New(
 		meld.NewHandler(
-			slog.NewJSONHandler(
-				writer,
-				&slog.HandlerOptions{
-					AddSource:   true,
-					Level:       level,
-					ReplaceAttr: gcp_logging.ReplaceAttr,
+			&motmedelLog.ContextHandler{
+				Handler: slog.NewJSONHandler(
+					writer,
+					&slog.HandlerOptions{
+						AddSource:   true,
+						Level:       level,
+						ReplaceAttr: gcp_logging.ReplaceAttr,
+					},
+				),
+				Extractors: []motmedelLog.ContextExtractor{
+					motmedelLog.ErrorContextExtractor,
+					&motmedelHttpLog.HttpContextExtractor{},
+					gcp_logging.HttpContextExtractor,
 				},
-			),
+			},
 		),
 	)
 
