1b39756d828658aa23bdf3c2d524dfcc4db8dd05
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-07-05 17:18:48 +0200

    Add auth client, refactor

--- pkg/auth/errors/errors.go
+++ pkg/auth/errors/errors.go
@@ -5,4 +5,7 @@ import "errors"
 var (
 	ErrNilCredentials = errors.New("nil credentials")
 	ErrNilToken       = errors.New("nil token")
+	ErrEmptyAccountKey              = errors.New("empty account key")
+	ErrEmptyImpersonateEmailAddress = errors.New("empty impersonate email address")
+	ErrNilAccountKeyConfig          = errors.New("nil account key config")
 )
