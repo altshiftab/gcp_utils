e684ebc9a6779a2a129938fa77a10a408efe7902
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-07-05 17:18:48 +0200

    Add auth client, refactor

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,33 +4,102 @@ import (
 	"context"
 	"fmt"
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
-	gcpUtilsAuthErrors "github.com/altshiftab/gcp_utils/pkg/auth/errors"
+	authErrors "github.com/altshiftab/gcp_utils/pkg/auth/errors"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"net/http"
 )
 
 const defaultCredentialsScope = "https://www.googleapis.com/auth/cloud-platform"
 
-func GetDefaultCredentialsTokenWithScope(scope string) (*oauth2.Token, error) {
-	credentials, err := google.FindDefaultCredentials(context.Background(), scope)
-	if err != nil {
-		return nil, motmedelErrors.MakeErrorWithStackTrace(
-			fmt.Errorf("find default credentials: %w", err),
-			scope,
-		)
-	}
-	if credentials == nil {
-		return nil, motmedelErrors.MakeErrorWithStackTrace(gcpUtilsAuthErrors.ErrNilCredentials)
+func GetDefaultCredentialsToken(ctx context.Context, scopes ...string) (*oauth2.Token, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, motmedelErrors.NewWithTrace(err, ctx)
 	}
 
+	if len(scopes) == 0 {
+		scopes = []string{defaultCredentialsScope}
+	}
+
+	credentials, err := google.FindDefaultCredentials(ctx, scopes...)
+	if err != nil {
+		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("find default credentials: %w", err))
+	}
+	if credentials == nil {
+		return nil, motmedelErrors.NewWithTrace(authErrors.ErrNilCredentials)
+	}
+
+	// TODO: Can I check if this interface is nil?
 	credentialsToken, err := credentials.TokenSource.Token()
 	if err != nil {
-		return nil, motmedelErrors.MakeErrorWithStackTrace(fmt.Errorf("token source token: %w", err))
+		return nil, motmedelErrors.New(fmt.Errorf("token source token: %w", err))
 	}
 
 	return credentialsToken, nil
 }
 
-func GetDefaultCredentialsToken() (*oauth2.Token, error) {
-	return GetDefaultCredentialsTokenWithScope(defaultCredentialsScope)
+func makeOauthClient(
+	ctx context.Context,
+	accountKey []byte,
+	impersonateEmailAddress string,
+	scopes ...string,
+) (*http.Client, error) {
+	if len(accountKey) == 0 {
+		return nil, motmedelErrors.NewWithTrace(authErrors.ErrEmptyAccountKey)
+	}
+
+	if err := ctx.Err(); err != nil {
+		return nil, motmedelErrors.NewWithTrace(err, ctx)
+	}
+
+	accountKeyConfig, err := google.JWTConfigFromJSON(accountKey, scopes...)
+	if err != nil {
+		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("google jwt config from json: %w", err))
+	}
+	if accountKeyConfig == nil {
+		return nil, motmedelErrors.NewWithTrace(authErrors.ErrNilAccountKeyConfig)
+	}
+
+	accountKeyConfig.Subject = impersonateEmailAddress
+
+	// TODO: Can I check if this interface is nil?
+	accountKeyToken, err := accountKeyConfig.TokenSource(ctx).Token()
+	if err != nil {
+		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("jwt config token source: %w", err))
+	}
+
+	return oauth2.NewClient(ctx, oauth2.StaticTokenSource(accountKeyToken)), nil
+}
+
+func MakeOauthClientFromAccountKey(ctx context.Context, accountKey []byte, scopes ...string) (*http.Client, error) {
+	if len(accountKey) == 0 {
+		return nil, nil
+	}
+
+	if err := ctx.Err(); err != nil {
+		return nil, motmedelErrors.NewWithTrace(err, ctx)
+	}
+
+	return makeOauthClient(ctx, accountKey, "", scopes...)
+}
+
+func MakeImpersonatedOauthClientFromAccountKey(
+	ctx context.Context,
+	accountKey []byte,
+	impersonateEmailAddress string,
+	scopes ...string,
+) (*http.Client, error) {
+	if len(accountKey) == 0 {
+		return nil, nil
+	}
+
+	if impersonateEmailAddress == "" {
+		return nil, motmedelErrors.NewWithTrace(authErrors.ErrEmptyImpersonateEmailAddress)
+	}
+
+	if err := ctx.Err(); err != nil {
+		return nil, motmedelErrors.NewWithTrace(err, ctx)
+	}
+
+	return makeOauthClient(ctx, accountKey, impersonateEmailAddress, scopes...)
 }
--- pkg/auth/go.mod
+++ pkg/auth/go.mod
@@ -1,10 +1,12 @@
 module github.com/altshiftab/gcp_utils/pkg/auth
 
-go 1.23.5
+go 1.24.0
+
+toolchain go1.24.4
 
 require (
-	github.com/Motmedel/utils_go v0.0.110
-	golang.org/x/oauth2 v0.26.0
+	github.com/Motmedel/utils_go v0.0.222
+	golang.org/x/oauth2 v0.30.0
 )
 
 require cloud.google.com/go/compute/metadata v0.3.0 // indirect
