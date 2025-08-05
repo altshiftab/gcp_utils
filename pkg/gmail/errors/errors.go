c646fe6c60cdc9134a3574b0ac95904e77febc41
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-08-05 15:50:09 +0200

    Add error

--- pkg/gmail/errors/errors.go
+++ pkg/gmail/errors/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrNilGmailService      = errors.New("nil gmail service")
 	ErrNilGmailUsersService = errors.New("nil gmail users service")
+	ErrNilGmailUsersMessagesService = errors.New("nil gmail users messages service")
 )
