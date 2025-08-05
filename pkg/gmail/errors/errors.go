6dc18832d135e5cd92f4639adc5566d255503b3b
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-08-05 18:51:23 +0200

    Fix errors

--- pkg/gmail/errors/errors.go
+++ pkg/gmail/errors/errors.go
@@ -3,7 +3,8 @@ package errors
 import "errors"
 
 var (
-	ErrNilGmailService      = errors.New("nil gmail service")
-	ErrNilGmailUsersService = errors.New("nil gmail users service")
-	ErrNilGmailUsersMessagesService = errors.New("nil gmail users messages service")
+	ErrNilService                   = errors.New("nil service")
+	ErrNilUsersService          = errors.New("nil users service")
+	ErrNilUsersMessagesService  = errors.New("nil users messages service")
+	ErrNilUsersMessagesSendCall = errors.New("nil users messages send call")
 )
