e29bf7888bbdea6a3ab654648b54e041ac6c0544
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-10-22 14:07:12 +0200

    Add infer extension id

---
+++ cmd/infer_extension_id/go.mod
@@ -0,0 +1,8 @@
+module github.com/altshiftab/gcp_utils/cmd/infer_extension_id
+
+go 1.25
+
+require (
+	github.com/Motmedel/utils_go v0.0.279
+	github.com/vphpersson/code_generation v0.0.9
+)
--- pkg/http/http.go
+++ pkg/http/http.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
+	motmedelHttpErrors "github.com/Motmedel/utils_go/pkg/http/errors"
 	motmedelMux "github.com/Motmedel/utils_go/pkg/http/mux"
 	motmedelMuxErrors "github.com/Motmedel/utils_go/pkg/http/mux/errors"
 	"github.com/Motmedel/utils_go/pkg/http/mux/types/endpoint_specification"
@@ -598,7 +599,7 @@ func MakeMux(
 	return mux
 }
 
-func PatchTrustedTypes(mux *motmedelMux.Mux, policies... string) error {
+func PatchTrustedTypes(mux *motmedelMux.Mux, policies ...string) error {
 	if mux == nil {
 		return motmedelErrors.NewWithTrace(motmedelMuxErrors.ErrNilMux)
 	}
@@ -616,7 +617,6 @@ func PatchTrustedTypes(mux *motmedelMux.Mux, policies... string) error {
 	requireTrustedTypesForDirective := &content_security_policy.RequireTrustedTypesForDirective{
 		SinkGroups: []string{"script"},
 	}
-
 
 	var expressions []content_security_policy.TrustedTypeExpression
 	for _, policy := range policies {
@@ -853,4 +853,83 @@ func MakeHttpService(
 	redirects ...[2]string,
 ) (*http.Server, *motmedelMux.Mux, error) {
 	return makeHttpService(domain, port, staticContentEndpointSpecifications, false, redirects...)
+}
+
+type CorsConfigurator struct {
+	AllowedOrigins   []string
+	RegisteredDomain string
+
+	Headers       []string
+	Credentials   bool
+	MaxAge        int
+	ExposeHeaders []string
+}
+
+func (configurator *CorsConfigurator) Parse(request *http.Request) (*motmedelHttpTypes.CorsConfiguration, *response_error.ResponseError) {
+	if request == nil {
+		return nil, &response_error.ResponseError{
+			ServerError: motmedelErrors.NewWithTrace(motmedelHttpErrors.ErrNilHttpRequest),
+		}
+	}
+
+	requestHeader := request.Header
+	if requestHeader == nil {
+		return nil, &response_error.ResponseError{
+			ServerError: motmedelErrors.NewWithTrace(motmedelHttpErrors.ErrNilHttpRequestHeader),
+		}
+	}
+
+	origin := requestHeader.Get("Origin")
+	if origin == "" {
+		return nil, nil
+	}
+
+	var matchedAllowedOrigin string
+	for _, allowedOrigin := range configurator.AllowedOrigins {
+		if strings.EqualFold(origin, allowedOrigin) {
+			matchedAllowedOrigin = allowedOrigin
+			break
+		}
+	}
+
+	registeredDomain := configurator.RegisteredDomain
+	if matchedAllowedOrigin == "" && registeredDomain != "" {
+		parsedOrigin, err := url.Parse(origin)
+		if err != nil {
+			return nil, &response_error.ResponseError{
+				ClientError: motmedelErrors.NewWithTrace(fmt.Errorf("url parse (origin): %w", err), origin),
+				ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+					"Invalid Origin header.",
+					nil,
+				),
+			}
+		}
+
+		originHostname := parsedOrigin.Hostname()
+		originDomainBreakdown := domain_breakdown.GetDomainBreakdown(originHostname)
+		if originDomainBreakdown == nil {
+			return nil, &response_error.ResponseError{
+				ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+					"Invalid Origin header hostname.",
+					nil,
+				),
+			}
+		}
+
+		if strings.EqualFold(originDomainBreakdown.RegisteredDomain, registeredDomain) {
+			matchedAllowedOrigin = origin
+		}
+	}
+
+	if matchedAllowedOrigin == "" {
+		return nil, nil
+	}
+
+	return &motmedelHttpTypes.CorsConfiguration{
+		Origin:        matchedAllowedOrigin,
+		Headers:       configurator.Headers,
+		Credentials:   configurator.Credentials,
+		MaxAge:        configurator.MaxAge,
+		ExposeHeaders: configurator.ExposeHeaders,
+	}, nil
 }
