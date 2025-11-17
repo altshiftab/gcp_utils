c48191a267cbe0b01924fdc05685ec41b97f99ca
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-11-17 17:29:33 +0100

    Refactor login

---
+++ pkg/http/login/sso/types/cse_config/cse_config.go
@@ -0,0 +1,66 @@
+package cse_config
+
+import (
+	"github.com/go-jose/go-jose/v4"
+)
+
+const (
+	DefaultClientPublicJwkHeader = "X-Client-Public-Jwk"
+	DefaultContentEncryption     = "A256GCM"
+	DefaultKeyAlgorithm          = "ECDH-ES"
+	DefaultKeyAlgorithmCurve     = "P-256"
+)
+
+var DefaultEncrypterOptions = (&jose.EncrypterOptions{}).WithContentType("application/json")
+
+type Option func(configuration *Config)
+
+type Config struct {
+	PrivateKey            any
+	ClientPublicJwkHeader string
+	KeyAlgorithm          jose.KeyAlgorithm
+	ContentEncryption     jose.ContentEncryption
+	EncrypterOptions      *jose.EncrypterOptions
+}
+
+func New(privateKey any, options ...Option) *Config {
+	config := &Config{
+		PrivateKey:            privateKey,
+		ClientPublicJwkHeader: DefaultClientPublicJwkHeader,
+		KeyAlgorithm:          DefaultKeyAlgorithm,
+		ContentEncryption:     DefaultContentEncryption,
+		EncrypterOptions:      DefaultEncrypterOptions,
+	}
+
+	for _, option := range options {
+		if option != nil {
+			option(config)
+		}
+	}
+
+	return config
+}
+
+func WithClientPublicJwkHeader(clientPublicJwkHeader string) Option {
+	return func(config *Config) {
+		config.ClientPublicJwkHeader = clientPublicJwkHeader
+	}
+}
+
+func WithContentEncryption(contentEncryption jose.ContentEncryption) Option {
+	return func(config *Config) {
+		config.ContentEncryption = contentEncryption
+	}
+}
+
+func WithKeyAlgorithm(keyAlgorithm jose.KeyAlgorithm) Option {
+	return func(config *Config) {
+		config.KeyAlgorithm = keyAlgorithm
+	}
+}
+
+func WithEncrypterOptions(encrypterOptions *jose.EncrypterOptions) Option {
+	return func(config *Config) {
+		config.EncrypterOptions = encrypterOptions
+	}
+}
