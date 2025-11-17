a99bc6862df5994a99a2a28e2ee977dc320bac95
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-11-17 13:25:46 +0100

    Add missing file

---
+++ pkg/http/login/sso/types/types.go
@@ -0,0 +1,10 @@
+package types
+
+type CallbackUrlInput struct {
+	State        string `json:"state"`
+	Code         string `json:"code"`
+	Scopes       string `json:"scopes"`
+	AuthUser     int    `json:"authuser"`
+	HostedDomain string `json:"hd"`
+	Prompt       string `json:"prompt"`
+}
