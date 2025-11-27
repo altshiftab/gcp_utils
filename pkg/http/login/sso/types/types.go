607558f3b879507ba16f92f1379213b721261d37
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-11-27 17:09:08 +0100

    Fix sso scopes

--- pkg/http/login/sso/types/types.go
+++ pkg/http/login/sso/types/types.go
@@ -2,9 +2,9 @@ package types
 
 type CallbackUrlInput struct {
 	State        string `json:"state"`
-	Code         string `json:"code"`
-	Scopes       string `json:"scopes"`
-	AuthUser     int    `json:"authuser"`
+	Code     string `json:"code"`
+	Scope    string `json:"scope"`
+	AuthUser int    `json:"authuser"`
 	HostedDomain string `json:"hd"`
 	Prompt       string `json:"prompt"`
 }
