8c0ba5347538954bba5c76a5834fafcd7345ec70
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-12-04 00:28:24 +0100

    Improve logger

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -1,55 +1,38 @@
 package metadata
 
 import (
-	"github.com/Motmedel/utils_go/pkg/errors"
+	"context"
+	"fmt"
+	"net/http"
+
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	motmedelHttpErrors "github.com/Motmedel/utils_go/pkg/http/errors"
 	motmedelHttpTypes "github.com/Motmedel/utils_go/pkg/http/types"
 	motmedelHttpUtils "github.com/Motmedel/utils_go/pkg/http/utils"
-	"net/http"
 )
 
 const metadataBaseUrlString = "http://metadata.google.internal/computeMetadata/v1"
 
-func GetProjectId(httpClient motmedelHttpUtils.HttpClient) (string, *motmedelHttpTypes.HttpContext, error) {
-	if httpClient == nil {
-		return "", nil, motmedelHttpErrors.ErrNilHttpClient
+func GetProjectId(ctx context.Context, httpClient *http.Client) (string, error) {
+	if err := ctx.Err(); err != nil {
+		return "", fmt.Errorf("context err: %w", err)
 	}
 
-	requestMethod := http.MethodGet
-	requestUrl := metadataBaseUrlString + "/project/project-id"
+	if httpClient == nil {
+		return "", motmedelErrors.NewWithTrace(motmedelHttpErrors.ErrNilHttpClient)
+	}
 
-	httpContext, err := motmedelHttpUtils.SendRequest(
+	_, responseBody, err := motmedelHttpUtils.Fetch(
+		ctx,
+		metadataBaseUrlString+"/project/project-id",
 		httpClient,
-		requestMethod,
-		requestUrl,
-		nil,
-		func(request *http.Request) error {
-			if request == nil {
-				return motmedelHttpErrors.ErrNilHttpRequest
-			}
-
-			requestHeader := request.Header
-			if requestHeader == nil {
-				return motmedelHttpErrors.ErrNilHttpRequestHeader
-			}
-
-			requestHeader.Set("Metadata-Flavor", "Google")
-
-			return nil
+		&motmedelHttpTypes.FetchOptions{
+			Headers: map[string]string{"Metadata-Flavor": "Google"},
 		},
 	)
 	if err != nil {
-		return "", httpContext, &errors.Error{
-			Message: "An error occurred when sending the request.",
-			Cause:   err,
-			Input:   []any{requestMethod, requestUrl},
-		}
-	}
-	if httpContext == nil {
-		return "", nil, motmedelHttpErrors.ErrNilHttpContext
+		return "", fmt.Errorf("http utils fetch: %w", err)
 	}
 
-	responseBody := httpContext.ResponseBody
-
-	return string(responseBody), httpContext, nil
+	return string(responseBody), nil
 }
