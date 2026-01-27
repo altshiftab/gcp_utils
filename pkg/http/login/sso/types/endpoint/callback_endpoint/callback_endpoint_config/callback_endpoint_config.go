ef99fcadbd19e007697e2c041583aa21e08daeb9
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-01-27 15:14:58 +0100

    Add missing file

---
+++ pkg/http/login/sso/types/endpoint/callback_endpoint/callback_endpoint_config/callback_endpoint_config.go
@@ -0,0 +1,28 @@
+package callback_endpoint_config
+
+var (
+	DefaultCallbackCookieName = "callback"
+)
+
+type Config struct {
+	CallbackCookieName string
+}
+
+type Option func(*Config)
+
+func New(options ...Option) *Config {
+	config := &Config{
+		CallbackCookieName: DefaultCallbackCookieName,
+	}
+	for _, option := range options {
+		option(config)
+	}
+
+	return config
+}
+
+func WithCallbackCookieName(callbackCookieName string) Option {
+	return func(config *Config) {
+		config.CallbackCookieName = callbackCookieName
+	}
+}
