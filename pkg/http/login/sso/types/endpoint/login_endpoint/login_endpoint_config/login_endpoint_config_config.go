9ce228c4ba18d1e5dd4f910e0defca9a06ab401c
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-01-28 12:00:07 +0100

    Fix callback cookie duration

--- pkg/http/login/sso/types/endpoint/login_endpoint/login_endpoint_config/login_endpoint_config_config.go
+++ pkg/http/login/sso/types/endpoint/login_endpoint/login_endpoint_config/login_endpoint_config_config.go
@@ -1,39 +1,28 @@
 package login_endpoint_config
 
 import (
-	"time"
-
 	"github.com/altshiftab/gcp_utils/pkg/http/login/sso/types/endpoint/callback_endpoint/callback_endpoint_config"
 )
 
 var (
-	DefaultCallbackCookieName     = callback_endpoint_config.DefaultCallbackCookieName
-	DefaultCallbackCookieDuration = 8 * time.Minute
+	DefaultCallbackCookieName = callback_endpoint_config.DefaultCallbackCookieName
 )
 
 type Config struct {
-	CallbackCookieDuration time.Duration
-	CallbackCookieName     string
+	CallbackCookieName string
 }
 
 type Option func(*Config)
 
 func New(options ...Option) *Config {
 	config := &Config{
-		CallbackCookieDuration: DefaultCallbackCookieDuration,
-		CallbackCookieName:     DefaultCallbackCookieName,
+		CallbackCookieName: DefaultCallbackCookieName,
 	}
 	for _, option := range options {
 		option(config)
 	}
 
 	return config
-}
-
-func WithCallbackCookieDuration(callbackCookieDuration time.Duration) Option {
-	return func(config *Config) {
-		config.CallbackCookieDuration = callbackCookieDuration
-	}
 }
 
 func WithCallbackCookieName(callbackCookieName string) Option {
