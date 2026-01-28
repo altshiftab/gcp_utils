b7445c3a28a7142196a55e233a498d50aceabaf6
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-01-28 23:25:23 +0100

    Add flow id

--- pkg/http/login/database/types/oauth_flow/oauth_flow.go
+++ pkg/http/login/database/types/oauth_flow/oauth_flow.go
@@ -3,6 +3,7 @@ package oauth_flow
 import "time"
 
 type Flow struct {
+	Id           string     `json:"id" postgres:"id,primarykey,type:uuid,default:gen_random_uuid()"`
 	CodeVerifier string     `postgres:"code_verifier,unique"`
 	State        string     `postgres:"state"`
 	ExpiresAt    *time.Time `postgres:"expires_at"`
