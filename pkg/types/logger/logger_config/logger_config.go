3455056cc3a4c59e9b8359e947a08f9845a1e8bb
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-02-21 15:49:17 +0100

    Rework logger

---
+++ pkg/types/logger/logger_config/logger_config.go
@@ -0,0 +1,69 @@
+package logger_config
+
+import (
+	"io"
+	"log/slog"
+	"os"
+
+	gcpHttpContextExtractor "github.com/Motmedel/utils_go/pkg/cloud/gcp/types/http_context_extractor"
+	"github.com/Motmedel/utils_go/pkg/http/types/http_context_extractor"
+)
+
+const (
+	DefaultLogLevel = slog.LevelInfo
+)
+
+var (
+	DefaultWriter               = os.Stdout
+	DefaultHttpContextExtractor = http_context_extractor.New()
+	DefaultGcpLoggingExtractor  = gcpHttpContextExtractor.New()
+)
+
+type Config struct {
+	Writer                  io.Writer
+	LogLevel                slog.Level
+	HttpContextExtractor    *http_context_extractor.Extractor
+	GcpHttpContextExtractor *gcpHttpContextExtractor.Extractor
+}
+
+type Option func(*Config)
+
+func New(options ...Option) *Config {
+	config := &Config{
+		Writer:                  DefaultWriter,
+		LogLevel:                DefaultLogLevel,
+		HttpContextExtractor:    DefaultHttpContextExtractor,
+		GcpHttpContextExtractor: DefaultGcpLoggingExtractor,
+	}
+	for _, option := range options {
+		if option != nil {
+			option(config)
+		}
+	}
+
+	return config
+}
+
+func WithWriter(writer io.Writer) Option {
+	return func(config *Config) {
+		config.Writer = writer
+	}
+}
+
+func WithLogLevel(logLevel slog.Level) Option {
+	return func(config *Config) {
+		config.LogLevel = logLevel
+	}
+}
+
+func WithHttpContextExtractor(httpContextExtractor *http_context_extractor.Extractor) Option {
+	return func(config *Config) {
+		config.HttpContextExtractor = httpContextExtractor
+	}
+}
+
+func WithGcpLoggingExtractor(gcpLoggingExtractor *gcpHttpContextExtractor.Extractor) Option {
+	return func(config *Config) {
+		config.GcpHttpContextExtractor = gcpLoggingExtractor
+	}
+}
